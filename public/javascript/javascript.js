f6f9bd7c87aaee3fa40e291ab4a3b2d0955943e6
zyckros <zyckros@gmail.com>
2017-08-05 22:06:14 +0100

    ultimas modificaciones

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -1,44 +1,37 @@
-$(document).ready(function(){
-    $("#fecha").click(function(){
-        $(this).hide(); 
-    });
-});
-
-function show5(){
-        if (!document.layers&&!document.all&&!document.getElementById)
+function show5() {
+    if (!document.layers && !document.all && !document.getElementById)
         return
 
-         var Digital=new Date()
-         var hours=Digital.getHours()
-         var minutes=Digital.getMinutes()
-         var seconds=Digital.getSeconds()
+    var Digital = new Date()
+    var hours = Digital.getHours()
+    var minutes = Digital.getMinutes()
 
-        var dn="PM"
-        if (hours<12)
-        dn="AM"
-        if (hours>12)
-        hours=hours-12
-        if (hours==0)
-        hours=12
+    var dn = "PM"
+    if (hours < 12)
+        dn = "AM"
+    if (hours > 12)
+        hours = hours - 12
+    if (hours == 0)
+        hours = 12
 
-         if (minutes<=9)
-         minutes="0"+minutes
-         if (seconds<=9)
-         seconds="0"+seconds
-        //change font size here to your desire
-        myclock="<font size='10'><font size='1'></font></br>"+hours+":"+minutes+":"
-         +seconds+" "+dn+"</b></font>"
-        if (document.layers){
+    if (minutes <= 9)
+        minutes = "0" + minutes
+    
+    //change font size here to your desire
+    
+    myclock = "<font size='10'><font size='1'></font></br>" + hours + ":" + minutes  +
+         " " + dn + "</b></font>"
+    if (document.layers) {
         document.layers.liveclock.document.write(myclock)
         document.layers.liveclock.document.close()
-        }
-        else if (document.all)
-        liveclock.innerHTML=myclock
-        else if (document.getElementById)
-        document.getElementById("liveclock").innerHTML=myclock
-        setTimeout("show5()",1000)
-         }
+    }
+    else if (document.all)
+        liveclock.innerHTML = myclock
+    else if (document.getElementById)
+        document.getElementById("liveclock").innerHTML = myclock
+    setTimeout("show5()", 1000)
+}
 
-/**/
-        window.onload=show5
-         
+
+window.onload = show5
+
